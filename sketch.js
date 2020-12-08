4348d6165e6d6468e2138530deb36601a967a115
AARAV-BHASIN-2177 <73882779+AARAV-BHASIN-2177@users.noreply.github.com>
2020-12-08 16:33:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,20 +33,17 @@ function draw() {
 
   
   
-  if(hasCollided(bullet,wall))
-  {
+  if(hasCollided(bullet,wall)){
     bullet.velocityX=0;
     var damage=0.5 *weight *speed *speed/(thickness * thickness * thickness)
-    if(damage>10)
-    {
+    if(damage>10){
       wall.shapeColor=color(255,0,0);
     }
 
-    if(damage<10)
-    {
+    if(damage<10){
       wall.shapeColor=color(0,255,0);
   }
-
+  }
   
   drawSprites();
 }
